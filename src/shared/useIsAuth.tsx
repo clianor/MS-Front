477350eb40f99780e1ad804bbd9600176460dda2
clianor <ejrgus94160@gmail.com>
2020-09-22 15:54:07 +0900

    useIsAuth 불필요 기능 삭제
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- src/shared/useIsAuth.tsx
+++ src/shared/useIsAuth.tsx
@@ -1,30 +1,22 @@
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {useRouter} from "next/router";
 import {useEffect} from "react";
-import {State} from "../store";
-import {meAction, meSetAction, MeState} from "../reducer/auth/me";
+import {MeState, meSetAction} from "../reducer/auth/me";
 
 // isAuth  null: 모두 허용, true: 로그인 유저만 허용, false: 로그인 유저는 접속 불가
-export const useIsAuth = (isAuth: boolean | null = null, state?: MeState) => {
+export const useIsAuth = (isAuth: boolean | null = null, meState: MeState) => {
   const router = useRouter();
   const dispatch = useDispatch();
-  const {me} = useSelector((state: State) => state.auth);
+  const isMe = meState.id ? true : false;
 
   useEffect(() => {
-    if (state) {
-      dispatch(meSetAction(state))
-    } else {
-      dispatch(meAction());
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isAuth === true && !me.id) {
+    if (isAuth === true && isMe === false) {
       // 로그인하지 않은 상태라면 로그인으로 보냄
       router.replace("/auth/login?next=" + router.pathname);
-    } else if (isAuth === false && !!me.id) {
+    } else if (isAuth === false && isMe === true) {
       // 로그인 유저는 접속 불가
       router.push("/");
     }
-  }, [me.id]);
+    dispatch(meSetAction(meState))
+  }, []);
 };
